621d924b7f777106046c2b11fcd3cf3cc0715cc8
TsangKalung <tsangkalung@bupt.edu.cn>
2020-11-24 13:48:27 +0800

    init

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer className="my-12 text-center">
+      © {new Date().getFullYear()}, Built by Tsang.
+    </footer>
+  )
+}
+
+export default Footer
